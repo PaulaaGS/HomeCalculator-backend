528c96803d388aa9041b4d739d7cd70e58031b3a
PaulaaGS <125793097+PaulaaGS@users.noreply.github.com>
2023-04-01 20:45:44 +0200

    Database configuration (#1)
    * Install and configure packages
    
    * Add empty expense module with controller and provider
    
    * Create expense entity
    
    * Use enums for order statuses and units
    
    * Use repository pattern

---
+++ src/expense/expense.entity.ts
@@ -0,0 +1,69 @@
+import { OrderStatus } from 'src/enums/order-status';
+import { Unit } from 'src/enums/unit';
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Expense {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({
+        length: 60,
+    })
+    name: string;
+
+    @Column({
+        length: 10000,
+        default: null,
+        nullable: true,
+    })
+    description: string | null;
+
+    @Column({
+        type:'enum',
+        enum: Unit,
+        default: Unit.PIECE,
+    })
+    unit: Unit;
+
+    @Column({
+        type: 'float',
+        precision: 6,
+        scale: 2,
+    })
+    unitPriceNet: number;
+
+    @Column({
+        type: 'integer',
+    })
+    vatRate: number;
+
+    @Column({
+        type: 'float',
+        precision: 6,
+        scale: 2,
+    })
+    quantity: number;
+
+    @Column({
+        type: 'float',
+        precision: 6,
+        scale: 2,
+    })
+    paidAmount: number;
+
+    @Column({
+        type: 'enum',
+        enum: OrderStatus,
+        default: OrderStatus.NOT_ORDERED,
+    })
+    orderStatus: OrderStatus;
+
+    @Column({
+        length: 2048,
+        default: null,
+        nullable: true,
+    })
+    url: string;
+}
