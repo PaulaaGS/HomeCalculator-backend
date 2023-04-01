4f0cb0f355fc1e3d1d7953ea3448deb04c0c7245
PaulaaGS <125793097+PaulaaGS@users.noreply.github.com>
2023-04-01 21:32:00 +0200

    Add endpoint to get all expenses (#2)

--- src/expense/expense.service.ts
+++ src/expense/expense.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Expense } from './expense.entity';
+import { Repository } from 'typeorm';
+import { GetListOfExpensesResponse } from 'src/interfaces/expense';
 
 @Injectable()
-export class ExpenseService {}
+export class ExpenseService {
+    constructor(
+        @InjectRepository(Expense)
+        private expenseRepository: Repository<Expense>,
+    ) {}
+
+    async getExpenses(): Promise<GetListOfExpensesResponse> {
+        return await this.expenseRepository.find();
+    }
+}
