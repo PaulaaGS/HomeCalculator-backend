d644fb356fba116e8aadd4ac7218a10e27a3ad4c
Paulina <paulina.a.gaweda@gmail.com>
2023-04-01 20:02:59 +0200

    Add empty expense module with controller and provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ExpenseModule } from './expense/expense.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'home_calculator',
+      entities: ['dist/**/**.entity{.ts,.js}'],
+      bigNumberStrings: false,
+      logging: true,
+      synchronize: true,
+    }),
+    ExpenseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
