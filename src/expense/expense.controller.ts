14073a56663d34d9fb9d2ed139f861464829eaec
Paulina <paulina.a.gaweda@gmail.com>
2023-04-01 22:32:02 +0200

    Create endpoints for one expense

--- src/expense/expense.controller.ts
+++ src/expense/expense.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Inject } from '@nestjs/common';
 import { ExpenseService } from './expense.service';
-import { GetListOfExpensesResponse } from 'src/interfaces/expense';
+import { CreateExpenseResponse, GetListOfExpensesResponse, GetOneExpenseResponse, UpdateExpenseResponse } from 'src/interfaces/expense';
+import { Body, Delete, Param, Post, Put } from '@nestjs/common/decorators';
+import { Expense } from './expense.entity';
 
 @Controller('expense')
 export class ExpenseController {
@@ -12,5 +14,34 @@ export class ExpenseController {
     @Get('/')
     getListOfExpenses(): Promise<GetListOfExpensesResponse> {
         return this.expenseService.getExpenses();
+    }
+
+    @Get('/:id')
+    getOneExpense(
+        @Param('id') id: string,
+    ): Promise<GetOneExpenseResponse> {
+        return this.expenseService.getOneExpense(id);
+    }
+
+    @Delete('/:id')
+    removeExpense(
+        @Param('id') id: string,
+    ): Promise<void> {
+        return this.expenseService.removeExpense(id);
+    }
+
+    @Post('/')
+    createExpense(
+        @Body() newExpense: Expense
+    ): Promise<CreateExpenseResponse> {
+        return this.expenseService.createExpense(newExpense);
+    }
+
+    @Put('/:id')
+    updateExpense(
+        @Param('id') id: string,
+        @Body() updatedExpense: Expense
+    ): Promise<UpdateExpenseResponse> {
+        return this.expenseService.updateExpense(id, updatedExpense);
     }
 }
