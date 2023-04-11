582e0380be3b1a357e77daac5196ea797f89dc6d
Paulina <paulina.a.gaweda@gmail.com>
2023-04-11 13:54:21 +0200

    Add category column to entity

--- src/expense/expense.service.ts
+++ src/expense/expense.service.ts
@@ -19,6 +19,7 @@ export class ExpenseService {
   async getExpenses(): Promise<GetListOfExpensesResponse> {
     return await this.expenseRepository.find({
       order: {
+        category: 'ASC',
         createdAt: 'ASC',
       },
     });
